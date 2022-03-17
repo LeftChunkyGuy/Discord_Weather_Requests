2d74b55b7b309815451e6f81e9cb3edffbbe5fad
LeftChunkyGuy <achait77@gmail.com>
2022-03-16 23:22:53 -0400

    Added basic functionality where the bot gets a command including a city name and does an API call for the current temp.

--- Bot.py
+++ Bot.py
@@ -4,6 +4,7 @@ import os
 import requests
 import asyncio
 import logging
+import json
 
 from os import path
 from discord.ext import commands, tasks
@@ -12,12 +13,26 @@ from dotenv import load_dotenv
 # pulling sensitive information from a .env file instead of hardcoding it in
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+APPID = os.getenv("WEATHER_APPID")
 
 # declare the bot as a client
 client = discord.Client()
 
-
+@client.event
+async def on_message(message):
+    if message.content.startswith('!weather'):
+        msg = message.content.split(" ")
+        city = msg[1]
+        response = requests.get(F'http://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={APPID}')
+        status_code = response.status_code
+        print(status_code)
+        if status_code == 200:
+            json_obj = response.json()
+            for key in json_obj:
+                if key == 'main':
+                    await message.channel.send(F'The weather in {city} right now is {json_obj[key]["temp"]} degrees.')
+        else:
+            await message.channel.send(F'{city} does not exist you fucking bafoon')
 
 @client.event
 async def on_ready():
